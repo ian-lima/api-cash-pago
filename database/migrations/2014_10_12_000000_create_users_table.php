307b9e779f8bc20b82a79057226c05e9e6984017
Ian Lima <ianmarialima@gmail.com>
2022-09-30 01:05:04 -0300

    Fix: inserindo dados automáticos na migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -23,7 +24,11 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken()->nullable();
             $table->timestamps();
-        });
+        }); 
+            DB::table('users')->insert([
+            'cpf' => '12345678911',
+            'password' => '123456'
+        ]);
     }
 
     /**
